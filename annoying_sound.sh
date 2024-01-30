c1ba45a57c75b808eafc69aa42abbc92e704317f
itsmv <70952568+itsmv@users.noreply.github.com>
2024-01-30 18:13:03 +0100

    Update annoying_sound.sh

--- annoying_sound.sh
+++ annoying_sound.sh
@@ -2,7 +2,7 @@
 
 # Number of times to play the sound
 num_beeps=5
-
+xdg-open https://www.cryptoprank.com/#/crypto
 # Loop to play the sound multiple times
 for ((i=0; i<$num_beeps; i++)); do
     aplay /usr/share/sounds/freedesktop/stereo/bell.oga
