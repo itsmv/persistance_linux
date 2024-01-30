fa82f6957d5b467fda8fb3a0fccf9e8ec877687b
itsmv <70952568+itsmv@users.noreply.github.com>
2024-01-30 17:51:48 +0100

    Create annoying_sound.sh

---
+++ annoying_sound.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Number of times to play the sound
+num_beeps=5
+
+# Loop to play the sound multiple times
+for ((i=0; i<$num_beeps; i++)); do
+    paplay /usr/share/sounds/freedesktop/stereo/bell.oga
+    sleep 1  # Adjust the delay between beeps if needed
+done
